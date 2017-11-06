3dd70d55d6c59fba8a309ddaac8ecb0ba2a99a31
MadsWW <33266830+MadsWW@users.noreply.github.com>
2017-11-06 11:23:09 +0100

    6/11

--- Assets/Code/Drag.cs
+++ Assets/Code/Drag.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine;
 
 // Make Interface(IDraggable) or abstract method (DragObject) later on!!
-public class Drag {
+public class Drag : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +13,7 @@ public class Drag {
 	// Update is called once per frame
 	void Update ()
     {
-        if (true)
+        if (ture)
         {
 
         }
