756ac316ab3b6949bc64c72f9563d6a8b5e30140
MadsWW <33266830+MadsWW@users.noreply.github.com>
2018-01-29 14:30:45 +0100

    f

--- Assets/Code/HoldLevelItems.cs
+++ Assets/Code/HoldLevelItems.cs
@@ -6,13 +6,6 @@ public class HoldLevelItems : MonoBehaviour {
 
     private List<Item> HighlightableItems = new List<Item>();
 
-    private GameManager gManager;
-    private Zoom zoom;
-
-
-    [HideInInspector]
-    public bool isActive = false;
-
     private void Start ()
     {
         gManager = FindObjectOfType<GameManager>();
@@ -25,23 +18,6 @@ public class HoldLevelItems : MonoBehaviour {
         foreach (Item item in GetComponentsInChildren<Item>())
         {
             HighlightableItems.Add(item);
-        }
-
-        Debug.Log(HighlightableItems.Count);
-    }
-
-    public void SendItemList()
-    {
-        if (isActive)
-        {
-            gManager.AddItemsToActiveList(HighlightableItems);
-            zoom.SetActiveLevel(gameObject);
-
-        }
-        else if (!isActive)
-        {
-            gManager.EmptyActiveItemList();
-            zoom.EmpyActiveLevel();
         }
     }
 }
