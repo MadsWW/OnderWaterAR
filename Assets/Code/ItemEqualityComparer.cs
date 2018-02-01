960218bd98c08108158711ea4a785a7e88778e51
MadsWW <33266830+MadsWW@users.noreply.github.com>
2018-02-01 21:39:52 +0100

    1

--- Assets/Code/ItemEqualityComparer.cs
+++ Assets/Code/ItemEqualityComparer.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemEqualityComparer : EqualityComparer<Item> {
+public class ItemEqualityComparer : EqualityComparer<Item>
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override bool Equals(Item x, Item y)
+    {
+        return Equals(x, y);
+    }
+
+    public override int GetHashCode(Item obj)
+    {
+        return obj.GetHashCode();
+    }
 }
